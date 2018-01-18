b773bd96ca07dbbc3005747c49e6e896a90bc44a
Rachel Ralston <rachelralston@gmail.com>
2018-01-18 10:40:43 -0800

    basic giphy api call working

--- giphy-api/server.js
+++ giphy-api/server.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch')
 const { MessagingResponse } = require('twilio').twiml
+
+const giphyAPIKey = process.env.GIPHY_API_KEY
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: false}));
@@ -9,11 +13,16 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.post('/sms', (req, res) => {
   const { Body } = req.body
   const twiml = new MessagingResponse()
+  const url = `http://api.giphy.com/v1/gifs/translate?api_key=${giphyAPIKey}&s=${Body}`
   
-  twiml.message('haii: ', Body)
-
-  res.writeHead(200, {'Content-Type': 'text/xml'})
-  res.end(twiml.toString())
+  fetch(url)
+    .then(response => response.json())
+    .then(payload => {
+      twiml.message(payload.data.images.looping.mp4)
+      res.writeHead(200, {'Content-Type': 'text/xml'})
+      res.end(twiml.toString())
+    })
+    .catch(e => console.error(e))
 })
 
 http.createServer(app).listen(3000, () => console.log('connected on port 3k 😺'))
